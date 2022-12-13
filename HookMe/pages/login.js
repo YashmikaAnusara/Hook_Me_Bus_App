a465b0cbaa4f3ba4206ab0a4ab3b040ca45cbaa7
Yashmika Anusara <yashmikaaunusara@gmail.com>
2022-12-13 20:46:26 +0530

    bug fix

--- HookMe/pages/login.js
+++ HookMe/pages/login.js
@@ -1,7 +1,6 @@
-import {View, Text, SafeAreaView, ScrollView, Image} from 'react-native';
+import {View, Text, SafeAreaView, Image, Pressable} from 'react-native';
 import React from 'react';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
-import RadialGradient from 'react-native-radial-gradient';
 
 const Darkmap = [
   {
@@ -191,30 +190,55 @@ const Darkmap = [
 ];
 export default function Login() {
   return (
-    <SafeAreaView style={{width: '100%', height: '100%'}}>
-      {/* <ScrollView> */}
-      <View style={{height: '100%', width: '100%', position: 'relative'}}>
+    <SafeAreaView style={{width: '100%', height: '100%', flex: 1}}>
+      <View
+        style={{
+          height: '100%',
+          width: '100%',
+          position: 'relative',
+          flex: 1,
+        }}>
+        <View style={{paddingTop: 110}}>
+          <MapView
+            style={{width: 500, height: 500}}
+            provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+            customMapStyle={Darkmap}
+            initialRegion={{
+              latitude: 6.914656,
+              longitude: 79.972773,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
+        </View>
         <Image
-          source={require('../assets/back2.png')}
+          source={require('../assets/back3.png')}
           style={{width: '100%', height: '100%', position: 'absolute'}}
         />
-        <View style={{paddingTop: 30, paddingLeft: 20}}>
+        <View
+          style={{
+            paddingTop: 40,
+            paddingLeft: 20,
+            position: 'absolute',
+          }}>
           <Text style={{color: '#ffffff', fontSize: 28}}>Welcome Thivaka</Text>
           <Text style={{color: '#ffffff', marginTop: 5, fontSize: 15}}>
             Trusted Travel Platform
           </Text>
+          <View>
+            <Image
+              source={require('../assets/profile.png')}
+              style={{
+                position: 'absolute',
+                marginLeft: 300,
+                marginTop: -62,
+                width: 50,
+                height: 50,
+              }}
+            />
+          </View>
         </View>
-        {/* <MapView
-          style={{width: 500, height: 500}}
-          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-          customMapStyle={Darkmap}
-          initialRegion={{
-            latitude: 6.914656,
-            longitude: 79.972773,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        /> */}
+
         <Image
           source={require('../assets/bus1.jpg')}
           style={{
@@ -225,8 +249,60 @@ export default function Login() {
             marginLeft: 140,
           }}
         />
+        <View
+          style={{
+            paddingTop: 540,
+            paddingLeft: 10,
+            paddingRight: 190,
+            position: 'absolute',
+          }}>
+          <Text
+            style={{
+              color: '#ffffff',
+              textAlign: 'center',
+              fontSize: 15,
+              lineHeight: 23,
+            }}>
+            We are provide the Private Buses & Sri Lanka Transport Board Buses
+          </Text>
+          <View style={{paddingTop: 10, paddingRight: 40, paddingLeft: 40}}>
+            <Pressable
+              style={{
+                backgroundColor: '#ffffff',
+                height: 30,
+                borderBottomEndRadius: 10,
+                borderBottomLeftRadius: 10,
+                borderTopLeftRadius: 10,
+                borderTopRightRadius: 10,
+              }}>
+              <Text
+                style={{textAlign: 'center', marginTop: 4, fontWeight: 'bold'}}>
+                About Us
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+        <View
+          style={{
+            width: '100%',
+            backgroundColor: '#525252',
+            marginTop: 70,
+            height: '100%',
+            borderTopLeftRadius: 60,
+            borderTopRightRadius: 60,
+          }}>
+          <View>
+            <Image
+              source={require('../assets/Home.png')}
+              style={{marginLeft: 40, marginTop: 20, height: 30, width: 30}}
+            />
+            <Text
+              style={{color: '#ffffff', fontWeight: 'bold', marginLeft: 35}}>
+              Home
+            </Text>
+          </View>
+        </View>
       </View>
-      {/* </ScrollView> */}
     </SafeAreaView>
   );
 }
