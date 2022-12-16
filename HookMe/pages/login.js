8ca11d1a9df94e74be76d38d102480bf601090bc
Yashmika Anusara <yashmikaaunusara@gmail.com>
2022-12-16 13:50:11 +0530

    Update login.js

--- HookMe/pages/login.js
+++ HookMe/pages/login.js
@@ -1,28 +1,94 @@
-import {View, Text, SafeAreaView, Image, TextInput} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
 
 export default function Login() {
   return (
-    <SafeAreaView>
-      <View>
-        <Image
-          source={require('../assets/back4.png')}
-          style={{width: '100%', height: '100%'}}
-        />
-        <View style={{position: 'absolute', paddingTop: 40, paddingLeft: 20}}>
-          <Text style={{color: '#ffffff', fontSize: 28}}>Welcome Hook Me</Text>
-          <Text style={{color: '#ffffff', fontSize: 15}}>
-            Trusted Travel Platform
-          </Text>
+    <SafeAreaView style={{flex: 1, height: '100%', width: '100%'}}>
+      <ScrollView>
+        <View
+          style={{
+            backgroundColor: '#262626',
+            flex: 1,
+          }}>
+          <View style={{position: 'absolute', paddingTop: 40, paddingLeft: 20}}>
+            <Text style={{color: '#ffffff', fontSize: 28}}>
+              Welcome Hook Me
+            </Text>
+            <Text style={{color: '#ffffff', fontSize: 15}}>
+              Trusted Travel Platform
+            </Text>
+          </View>
+          <View style={{position: 'absolute', paddingTop: 150}}>
+            <Image source={require('../assets/bus2.png')} />
+          </View>
+          <View style={{paddingTop: 470, paddingBottom: '40.5%'}}>
+            <TextInput
+              placeholder="Enter your username"
+              style={{
+                height: 40,
+                margin: 12,
+                borderWidth: 1,
+                padding: 10,
+                backgroundColor: '#ffffff',
+                borderBottomLeftRadius: 10,
+                borderBottomRightRadius: 10,
+                borderTopLeftRadius: 10,
+                borderTopRightRadius: 10,
+              }}
+            />
+            <TextInput
+              placeholder="Enter your password"
+              style={{
+                height: 40,
+                margin: 12,
+                borderWidth: 1,
+                padding: 10,
+                backgroundColor: '#ffffff',
+                borderBottomLeftRadius: 10,
+                borderBottomRightRadius: 10,
+                borderTopLeftRadius: 10,
+                borderTopRightRadius: 10,
+              }}
+            />
+          </View>
+          <View
+            style={{
+              position: 'absolute',
+              paddingTop: 620,
+              width: '100%',
+              paddingLeft: 30,
+              paddingRight: 30,
+            }}>
+            <TouchableOpacity
+              style={{
+                backgroundColor: '#A0A0A0',
+                height: 40,
+                borderTopLeftRadius: 20,
+                borderBottomLeftRadius: 20,
+                borderTopRightRadius: 20,
+                borderBottomRightRadius: 20,
+              }}>
+              <Text
+                style={{
+                  textAlign: 'center',
+                  paddingTop: 6,
+                  fontWeight: 'bold',
+                  fontSize: 20,
+                }}>
+                Sing in
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View style={{position: 'absolute', paddingTop: 150}}>
-          <Image source={require('../assets/bus2.png')} />
-        </View>
-        <TextInput
-          placeholder="Enter your username"
-          style={{color: '#ffffff', position: 'absolute'}}
-        />
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
