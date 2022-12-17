0a73e5eb05d46312a42ab0aa2a70173f8bec96fc
Yashmika Anusara <yashmikaaunusara@gmail.com>
2022-12-17 10:21:37 +0530

    Update signup.js

--- HookMe/pages/signup.js
+++ HookMe/pages/signup.js
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, SafeAreaView, ScrollView} from 'react-native';
+import React from 'react';
 
 export default function Signup() {
   return (
-    <View>
-      <Text>Signup</Text>
-    </View>
-  )
+    <SafeAreaView style={{flex: 1, height: '100%', width: '100%'}}>
+      <ScrollView>
+        <View
+          style={{
+            backgroundColor: '#262626',
+            flex: 1,
+          }}></View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
