51eccf59a81635b31eda880dde28c335b756d419
Yashmika Anusara <yashmikaaunusara@gmail.com>
2022-12-17 10:32:18 +0530

    bug fix

--- HookMe/pages/signup.js
+++ HookMe/pages/signup.js
@@ -1,7 +1,20 @@
-import {View, Text, SafeAreaView, ScrollView} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
+import {useNavigation} from '@react-navigation/core';
 
 export default function Signup() {
+  const navigation = useNavigation();
+
+  const Back = () => {
+    navigation.navigate('Login');
+  };
   return (
     <SafeAreaView style={{flex: 1, height: '100%', width: '100%'}}>
       <ScrollView>
@@ -9,7 +22,24 @@ export default function Signup() {
           style={{
             backgroundColor: '#262626',
             flex: 1,
-          }}></View>
+          }}>
+          <View style={{position: 'absolute', paddingTop: 45}}>
+            <TouchableOpacity onPress={Back}>
+              <Image
+                source={require('../assets/back.png')}
+                style={{width: 50, height: 50}}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={{paddingTop: 40, paddingLeft: 50}}>
+            <Text style={{color: '#ffffff', fontSize: 28}}>
+              Welcome Hook Me
+            </Text>
+            <Text style={{color: '#ffffff', fontSize: 15}}>
+              User Registration
+            </Text>
+          </View>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
