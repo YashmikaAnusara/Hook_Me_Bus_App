bd1f3cf8e4d5346bfa2786132bdd5f17a894cb3d
Yashmika Anusara <yashmikaaunusara@gmail.com>
2023-01-01 15:25:45 +0530

    Create aboutus.js

---
+++ HookMe/pages/aboutus.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function Aboutus() {
+  return (
+    <View>
+      <Text>Aboutus</Text>
+    </View>
+  );
+}
